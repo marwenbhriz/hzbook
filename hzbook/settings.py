23eb2cee0fe7cf53e1dfa2ec4adb43ad86d3a2b9
marwenbhriz <marwen.benhriz@gmail.com>
2023-09-08 10:10:29 +0900

    FIX MONGO

--- hzbook/settings.py
+++ hzbook/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'hzbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
+    },
+    'mongo': {
+        'ENGINE': 'djongo',
+        'NAME': 'hzbook',
+        'CLIENT': {
+            'host': 'mongodb+srv://jrcsuser:password@mongodb-service.default.svc.cluster.local',
+            'username': 'jrcsuser',
+            'password': 'password',
+        }
     }
 }
 
